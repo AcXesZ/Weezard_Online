a2a4e411d90a8f82c9ec86aa566f9621146a68dc
Carl <carltrubiano3@gmail.com>
2022-10-19 12:19:02 -0400

    display prod_table with default route

--- main_page.py
+++ main_page.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+import jinja2
+
 
 app = Flask(__name__, template_folder="templates")
 app.config.from_object(__name__)
 # configuration
-DATABASE = '/tmp/flaskr.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'admin'
